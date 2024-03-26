7a39d6731a6c10a9e032f3a569bf22a66cd8ed00
Woodie <quocduy060220@gmail.com>
2024-03-26 13:21:02 +0700

    feat: clean home page

--- components/main/header.vue
+++ components/main/header.vue
@@ -3,29 +3,22 @@
     <header class="header flex flex-row justify-between items-center mx-3rem">
       <div class="logo">
         <BlocLink linkTo="/">
-          <img class="logo-image" src="~/assets/images/logo.png"  alt="woodie logo">
+          <img
+            class="logo-image"
+            src="~/assets/images/logo.png"
+            alt="woodie logo"
+          />
         </BlocLink>
       </div>
       <div>
-        <BlocButton class="mr-1" :class="$route.path === '/' ? 'active' : ''">
-          <i class="mr-1 fa-solid fa-house"></i>
-          Home
-        </BlocButton>
-        <BlocButton class="mr-1" :class="$route.path === '/about' ? 'active' : ''">
-          <i class="mr-1 fa-solid fa-user"></i>
-          About
-        </BlocButton>
-        <BlocButton class="mr-1" :class="$route.path === '/resume' ? 'active' : ''">
-          <i class="mr-1 fa-solid fa-file"></i>
-          Resume
-        </BlocButton>
-        <BlocButton class="mr-1" :class="$route.path === '/works' ? 'active' : ''">
-          <i class="mr-1 fa-solid fa-briefcase"></i>
-          Works
-        </BlocButton>
-        <BlocButton class="mr-1" :class="$route.path === '/contact' ? 'active' : ''">
-          <i class="mr-1 fa-solid fa-address-book"></i>
-          Contact
+        <BlocButton
+          v-for="tab in tabs"
+          :key="tab.id"
+          class="mr-1"
+          :class="$route.path === tab.route ? 'active' : ''"
+        >
+          <i :class="tab.iconClass"></i>
+          {{ tab.title }}
         </BlocButton>
       </div>
     </header>
@@ -36,14 +29,47 @@
 import { Component, Vue } from 'nuxt-property-decorator';
 
 @Component
-export default class Header extends Vue {}
+export default class Header extends Vue {
+  tabs = [
+    {
+      id: 1,
+      route: '/',
+      iconClass: 'mr-1 fa-solid fa-house',
+      title: 'Home',
+    },
+    {
+      id: 2,
+      route: '/about',
+      iconClass: 'mr-1 fa-solid fa-user',
+      title: 'About',
+    },
+    {
+      id: 3,
+      route: '/resume',
+      iconClass: 'mr-1 fa-solid fa-file',
+      title: 'Resume',
+    },
+    {
+      id: 4,
+      route: '/works',
+      iconClass: 'mr-1 fa-solid fa-briefcase',
+      title: 'Works',
+    },
+    {
+      id: 5,
+      route: '/contact',
+      iconClass: 'mr-1 fa-solid fa-address-book',
+      title: 'Contact',
+    },
+  ];
+}
 </script>
 
 <style lang="scss">
-  .header {
-    @apply pt-4
-  }
-  .logo {
-    @apply max-w-140px;
-  }
+.header {
+  @apply pt-4;
+}
+.logo {
+  @apply max-w-140px;
+}
 </style>
