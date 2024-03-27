a2a9db517661bc9c5a6c613c33d0949c53d80d2e
Woodie <quocduy060220@gmail.com>
2024-03-27 14:23:53 +0700

    button styles

--- components/bloc/button.vue
+++ components/bloc/button.vue
@@ -17,7 +17,7 @@ export default class Button extends Vue {
 
 <style>
 .btn {
-  @apply font-light text-dark-600 dark:text-light-500 dark:bg-transparent py-2.5 px-2 border-0 rounded-lg bg-white text-sm transition-all;
+  @apply font-normal text-dark-600 dark:text-light-500 dark:bg-transparent py-2.5 px-2 border-0 rounded-lg bg-white text-sm transition-all;
 
   &:hover {
     background: linear-gradient(97deg, #fa5252, #dd2476);
