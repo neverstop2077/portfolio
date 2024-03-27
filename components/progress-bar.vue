43e04c062dd3f1c9aa72f2c86814d1ce26d63bfe
Woodie <quocduy060220@gmail.com>
2024-03-27 13:29:25 +0700

    feat: resume page

---
+++ components/progress-bar.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="progress" :class="progressClass">
+    <div class="flex justify-between mb-2">
+      <div class="progress-title">{{ title }}</div>
+      <div class="progress-percent">{{ percent }}%</div>
+    </div>
+    <div class="relative">
+      <div
+        class="progress-bar"
+        :class="color"
+        :style="{ width: `${percent}%` }"
+      ></div>
+      <div class="progress-base-bar"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { Component, Vue, Prop } from 'nuxt-property-decorator';
+
+@Component
+export default class ProgressBar extends Vue {
+  @Prop({ required: true, type: Number }) percent;
+  @Prop({ required: false, type: String }) title;
+  @Prop({ required: true, type: String }) color;
+  @Prop({ required: false, type: String }) progressClass;
+}
+</script>
+
+<style>
+.progress {
+  @apply relative flex flex-col;
+  &-title,
+  &-percent {
+    @apply text-normal text-zinc-600 dark:text-zinc-400;
+  }
+  &-bar {
+    @apply h-6px rounded-lg absolute z-10;
+  }
+  &-base-bar {
+    @apply w-full h-6px bg-zinc-200 rounded-lg relative;
+  }
+}
+</style>
--- pages/resume/index.vue
+++ pages/resume/index.vue
@@ -5,88 +5,72 @@
         <h1 class="headline-1">{{ getSection('hero.heading') }}</h1>
       </div>
       <div class="card-content">
-        <div class="flex flex-row columns-2 items-center">
-          <div class="card-content-item card-content-left">
-            <img
-              :src="getSection('hero.image')"
-              :alt="getSection('hero.imageAlt')"
-              class="summary-image"
-            />
-          </div>
-          <div class="card-content-item card-content-right">
-            <div class="card-content-section">
-              <h2 class="card-content-section-heading">
-                {{ getSection('hero.summary.heading') }}
-              </h2>
-              <p
-                class="card-content-section-paragraph"
-                v-for="content in getSection('hero.summary.content')"
-                :key="content.id"
-              >
-                {{ content.description }}
-              </p>
-            </div>
-            <div class="card-content-section">
-              <h2 class="card-content-section-heading">
-                {{ getSection('hero.information.heading') }}
-              </h2>
-              <div class="grid grid-cols-2">
+        <div class="mt-8">
+          <div class="resume-container">
+            <div
+              v-for="(resume, resumeIndex) in getSection('hero.resume')"
+              :key="resume.id"
+              class="resume-col"
+            >
+              <div class="resume-header">
+                <i :class="resume.icon"></i>
+                <h2 class="resume-title">{{ resume.title }}</h2>
+              </div>
+              <div class="resume-list">
                 <div
-                  v-for="badge in getSection('hero.information.badges')"
-                  :key="badge.id"
-                  class="badge"
+                  v-for="(item, itemIndex) in resume.items"
+                  :key="item.id"
+                  class="tag resume-tag"
+                  :class="
+                    (resumeIndex + itemIndex) % 2 === 0
+                      ? 'tag-pink'
+                      : 'tag-blue'
+                  "
                 >
-                  <BlocButton :buttonClass="badge.iconColor">
-                    <i :class="badge.iconClass"></i>
-                  </BlocButton>
-                  <div class="flex flex-col">
-                    <span class="badge-title">{{ badge.title }}</span>
-                    <span class="badge-content">{{ badge.content }}</span>
-                  </div>
+                  <p class="tag-timeline">{{ item.timeline }}</p>
+                  <h3 class="tag-title !mb-0 !font-normal !text-1.125rem">
+                    {{ item.title }}
+                  </h3>
+                  <p class="tag-content">{{ item.location }}</p>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <div class="mt-8">
-          <h2 class="card-content-section-heading">
-            {{ getSection('main.showCase.heading') }}
-          </h2>
-          <div class="grid grid-cols-2 gap-6 mt-8">
-            <div
-              v-for="(tag, index) in getSection('main.showCase.tags')"
-              class="tag"
-              :class="index % 2 ? 'tag-blue' : 'tag-pink'"
-              :key="tag.id"
-            >
-              <i :class="tag.tagIcon"></i>
+        <div
+          class="section-wrapper px-8 !items-stretch !flex-row !justify-between gap-x-8"
+        >
+          <div class="flex flex-row flex-1 max-w-3/5">
+            <div class="w-full">
+              <h2 class="card-content-section-heading !mb-6 !font-medium">
+                {{ getSection('main.skills.heading') }}
+              </h2>
               <div class="">
-                <h3 class="tag-title">{{ tag.tagTitle }}</h3>
-                <p class="tag-content">
-                  {{ tag.tagContent }}
-                </p>
+                <ProgressBar
+                  v-for="item in getSection('main.skills.list')"
+                  :key="item.id"
+                  progressClass="mb-8"
+                  :percent="item.percent"
+                  :title="item.title"
+                  :color="item.color"
+                />
               </div>
             </div>
           </div>
-        </div>
-        <div class="section-wrapper">
-          <h2 class="card-content-section-heading !mb-6">
-            {{ getSection('end.clients.heading') }}
-          </h2>
-          <carousel :autoplay="true" :per-page="1" :pagination-enabled="false">
-            <slide
-              v-for="client in getSection('end.clients.content')"
-              :key="client.id"
-            >
-              <div class="carousel-item">
-                <img
-                  class="carousel-image"
-                  :src="client.src"
-                  :alt="client.alt"
-                />
+          <div class="flex-1 max-w-2/5">
+            <h2 class="card-content-section-heading !mb-6 !font-medium">
+              {{ getSection('main.knowledge.heading') }}
+            </h2>
+            <div class="flex flex-row flex-wrap gap-x-3 gap-y-5">
+              <div
+                class="static-tag"
+                v-for="item in getSection('main.knowledge.list')"
+                :key="item.id"
+              >
+                <div class="static-tag-content">{{ item.title }}</div>
               </div>
-            </slide>
-          </carousel>
+            </div>
+          </div>
         </div>
       </div>
       <div class="card-footer">
@@ -107,128 +91,106 @@ export default class AboutPage extends mixins(BasePage) {
   section = {
     hero: {
       heading: 'Resume',
-      image: require('~/assets/images/summary-image.jpg'),
-      imageAlt: "woodie's image",
-      summary: {
-        heading: 'Who Am I?',
-        content: [
-          {
-            id: 1,
-            description:
-              "I'm a front-end developer based in Vietnam, where I specialize in crafting beautiful and user-friendly websites. I love simplifying complex issues to create straightforward solutions. My passion lies in building engaging online experiences that truly resonate with users.",
-          },
-          {
-            id: 2,
-            description:
-              "My goal is to bring your ideas to life in the most creative way possible. Having collaborated with numerous renowned brands, I've helped them shine online with visually stunning and intuitive designs. Let's collaborate to ensure your website stands out in the digital landscape!",
-          },
-        ],
-      },
-      information: {
-        heading: 'Personal Info',
-        badges: [
-          {
-            id: 1,
-            iconColor:
-              'badge-signature !hover:text-white !text-pink-500 !dark:bg-dark-900',
-            iconClass: 'fa-solid fa-mobile-screen-button badge-icon',
-            title: 'Phone',
-            content: '+84 846 905 006',
-          },
-          {
-            id: 2,
-            iconColor:
-              'badge-signature !hover:text-white !text-cyan-500 !dark:bg-dark-900',
-            iconClass: 'fa-solid fa-inbox badge-icon',
-            title: 'Email',
-            content: 'quocduy060220@gmail.com',
-          },
-          {
-            id: 3,
-            iconColor:
-              'badge-signature !hover:text-white !text-green-500 !dark:bg-dark-900',
-            iconClass: 'fa-solid fa-location-dot badge-icon',
-            title: 'Location',
-            content: 'Viet Nam',
-          },
-          {
-            id: 4,
-            iconColor:
-              'badge-signature !hover:text-white !text-orange-500 !dark:bg-dark-900',
-            iconClass: 'fa-solid fa-calendar-days badge-icon',
-            title: 'Birthday',
-            content: 'Feb 06, 2000',
-          },
-        ],
-      },
+      resume: [
+        {
+          id: 1,
+          title: 'Education',
+          icon: 'fa-solid fa-book resume-icon',
+          items: [
+            {
+              id: 1,
+              timeline: '2018-2023',
+              title: 'Bachelor IT - University',
+              location: 'Can Tho, Viet Nam',
+            },
+          ],
+        },
+        {
+          id: 2,
+          title: 'Experience',
+          icon: 'fa-solid fa-flask resume-icon ',
+          items: [
+            {
+              id: 1,
+              timeline: '2018-present',
+              title: 'Web Dev',
+              location: 'NFQ Vietnam',
+            },
+          ],
+        },
+        {
+          id: 3,
+          title: 'Projects',
+          icon: 'fa-solid fa-briefcase resume-icon ',
+          items: [
+            {
+              id: 1,
+              timeline: '2023-2024',
+              title: 'Scale Commerce',
+              location: 'NFQ Vietnam',
+            },
+            {
+              id: 2,
+              timeline: '2024-present',
+              title: 'Emma Career',
+              location: 'NFQ Vietnam',
+            },
+          ],
+        },
+      ],
     },
     main: {
-      showCase: {
-        heading: 'What I Do!',
-        tags: [
+      skills: {
+        heading: 'Working Skills',
+        list: [
           {
             id: 1,
-            tagIcon: 'tag-signature text-4xl fa-solid fa-code text-pink-400',
-            tagTitle: 'Web Development',
-            tagContent:
-              'I build websites with HTML, CSS, and JavaScript to make them work smoothly.',
+            title: 'HTML/CSS',
+            percent: 85,
+            color: 'bg-red-400',
           },
           {
             id: 2,
-            tagIcon: 'tag-signature text-4xl fa-solid fa-eye text-cyan-400',
-            tagTitle: 'Design Eyes',
-            tagContent:
-              'I make websites look great by choosing nice colors and arranging things neatly.',
+            title: 'Javascript',
+            percent: 77,
+            color: 'bg-cyan-400',
           },
           {
             id: 3,
-            tagIcon:
-              'tag-signature text-4xl fa-solid fa-hourglass-half text-orange-400',
-            tagTitle: 'Management',
-            tagContent:
-              'I keep things organized and make sure projects finish on time.',
+            title: 'VueJS',
+            percent: 70,
+            color: 'bg-indigo-400',
           },
           {
             id: 4,
-            tagIcon: 'tag-signature text-4xl fa-solid fa-comments text-red-400',
-            tagTitle: 'Communicate',
-            tagContent:
-              'I talk with clients and teammates to understand what they want.',
+            title: 'Git',
+            percent: 80,
+            color: 'bg-pink-400',
+          },
+        ],
+      },
+      knowledge: {
+        heading: 'Knowledge',
+        list: [
+          {
+            id: 1,
+            title: 'Web development',
+          },
+          {
+            id: 2,
+            title: 'Flexibility',
+          },
+          {
+            id: 3,
+            title: 'Time management',
+          },
+          {
+            id: 4,
+            title: 'Communication',
           },
           {
             id: 5,
-            tagIcon: 'tag-signature text-4xl fa-solid fa-wrench text-green-400',
-            tagTitle: 'Improve Ui/Ux',
-            tagContent: 'I make websites easy to use and fun to look at.',
-          },
-          {
-            id: 6,
-            tagIcon:
-              'tag-signature text-4xl fa-solid fa-people-group text-purple-400',
-            tagTitle: 'Team player',
-            tagContent: 'I work well with others and help my team succeed.',
-          },
-        ],
-      },
-    },
-    end: {
-      clients: {
-        heading: 'Clients',
-        content: [
-          {
-            id: 1,
-            src: require('~/assets/images/emma.png'),
-            alt: 'emma sleep logo',
-          },
-          {
-            id: 2,
-            src: require('~/assets/images/nfq-logo.png'),
-            alt: 'nfq logo',
-          },
-          {
-            id: 3,
-            src: require('~/assets/images/scalecommerce.png'),
-            alt: 'scale commerce logo',
+            title: 'Presentation',
           },
         ],
       },
@@ -239,17 +201,26 @@ export default class AboutPage extends mixins(BasePage) {
 </script>
 
 <style lang="scss">
-.summary-image {
-  @apply max-w-270px max-h-400px rounded-3xl;
-}
-
-.carousel {
-  &-item {
-    @apply flex items-center justify-center w-full max-h-100px;
+.resume {
+  &-container {
+    @apply grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-x-8 gap-y-12;
   }
-
-  &-image {
-    @apply max-h-70px;
+  &-header {
+    @apply flex items-center gap-x-2 mb-6;
+  }
+  &-title {
+    @apply inline text-2xl text-black dark:text-white;
+  }
+  &-icon {
+    @apply text-3xl text-red-400;
+  }
+  &-list {
+    @apply flex flex-col gap-y-2;
+  }
+  &-tag {
+    &.tag {
+      @apply flex flex-col gap-y-1;
+    }
   }
 }
 </style>
