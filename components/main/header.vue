08acf7575dd25c91dde2c439a95b0bd82c5d9769
Woodie <quocduy060220@gmail.com>
2024-03-28 13:27:04 +0700

    feat: header done

--- components/main/header.vue
+++ components/main/header.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container mx-auto">
-    <header class="header flex flex-row justify-between items-center mx-3rem">
+    <header class="header">
       <div class="logo">
         <BlocLink linkTo="/">
           <img
@@ -11,15 +11,18 @@
         </BlocLink>
       </div>
       <div class="flex flex-row items-center">
-        <BlocLink v-for="tab in tabs" :key="tab.id" :linkTo="tab.route">
-          <BlocButton
-            class="mr-3"
-            :class="$route.path === tab.route ? 'active' : ''"
-          >
-            <i :class="tab.iconClass"></i>
-            {{ tab.title }}
-          </BlocButton>
-        </BlocLink>
+        <!--Desktop Menu-->
+        <div class="hidden lg:block">
+          <BlocLink v-for="tab in tabs" :key="tab.id" :linkTo="tab.route">
+            <BlocButton
+              class="mr-3"
+              :class="$route.path === tab.route ? 'active' : ''"
+            >
+              <i :class="tab.iconClass"></i>
+              {{ tab.title }}
+            </BlocButton>
+          </BlocLink>
+        </div>
         <BlocButton
           buttonClass="color-mode-btn"
           @button-click="onColorModeChange"
@@ -30,6 +33,24 @@
           ></i>
           <i v-else class="fa-solid fa-moon"></i>
         </BlocButton>
+        <BlocButton
+          @button-click="mobileMenuOpen = !mobileMenuOpen"
+          buttonClass="burger-button active"
+        >
+          <i class="fa-solid fa-bars"></i>
+        </BlocButton>
+        <!--Mobile Menu-->
+        <div class="mobile-menu" :class="mobileMenuOpen ? 'active' : ''">
+          <BlocLink v-for="tab in tabs" :key="tab.id" :linkTo="tab.route">
+            <span
+              :class="$route.path === tab.route ? 'active' : ''"
+              class="mobile-menu-item"
+            >
+              <i :class="tab.iconClass"></i>
+              {{ tab.title }}
+            </span>
+          </BlocLink>
+        </div>
       </div>
     </header>
   </div>
@@ -75,6 +96,7 @@ export default class Header extends Vue {
       title: 'Contact',
     },
   ];
+  mobileMenuOpen = false;
 
   onColorModeChange = () => {
     this.$colorMode.preference =
@@ -85,13 +107,30 @@ export default class Header extends Vue {
 
 <style lang="scss">
 .header {
-  @apply pt-4;
+  @apply pt-4 flex flex-row justify-between items-center mx-3rem relative;
 }
 .logo {
   @apply max-w-140px;
 }
-.color-mode-btn {
+.color-mode-btn,
+.burger-button {
   @apply w-45px h-45px ml-4;
   border-radius: 50% !important;
+}
+.burger-button {
+  @apply block lg:hidden;
+}
+.mobile-menu {
+  @apply py-0 px-6 box-border bg-white dark:bg-black rounded-xl transform scale-y-0 origin-top shadow-sm dark:shadow-gray-800 absolute z-10 left-0 right-0 top-full flex flex-col gap-y-4 lg:hidden transition-all duration-600 overflow-hidden;
+  &.active {
+    @apply py-4 transform scale-y-100;
+  }
+  &-item {
+    @apply text-sm text-zinc-600 dark:text-zinc-400 hover:text-rose-500;
+
+    &.active {
+      @apply text-rose-500;
+    }
+  }
 }
 </style>
